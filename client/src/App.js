ba7df69e7c00ff9270235bc842390ce96d03fe11
victor soares <52243919+vmsoares@users.noreply.github.com>
2025-06-09 09:31:53 -0700

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Gamified Casual Game</h1>
+      <p>Bem‑vindo! O jogo está em construção.</p>
+    </div>
+  );
+}
+
+export default App;
